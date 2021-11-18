b5bbbe290ea8938d0390b785af0c3989f27f0970
Unero <20809454+un-ro@users.noreply.github.com>
2021-11-18 20:22:26 +0700

    Create exception card

---
+++ lib/ui/widget/exception_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class ExceptionCard extends StatelessWidget {
+  final String assetPath;
+  final String message;
+  const ExceptionCard({
+    Key? key,
+    required this.assetPath,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset(
+            assetPath,
+            height: 200.0,
+            width: 200.0,
+          ),
+          Text(message)
+        ],
+      ),
+    );
+  }
+}
