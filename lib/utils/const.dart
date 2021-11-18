50bc0e3df56e7f6d20236be5938d37648aa3047b
Unero <20809454+un-ro@users.noreply.github.com>
2021-11-18 20:18:07 +0700

    Create utlis files

---
+++ lib/utils/const.dart
@@ -0,0 +1,13 @@
+const String APP_NAME = 'GoNakam';
+const String APP_VERSION = '2.0.0';
+
+// API Related
+const String BASE_URL = 'https://restaurant-api.dicoding.dev';
+const String SMALL_IMAGE = '$BASE_URL/images/small/';
+const String MEDIUM_IMAGE = '$BASE_URL/images/medium/';
+const String LARGE_IMAGE = '$BASE_URL/images/large/';
+
+const String ERROR_RESPONSE =
+    'Something went wrong, check your internet connection';
+const String EMPTY_RESPONSE =
+    'Empty Result from server, check again your search';
