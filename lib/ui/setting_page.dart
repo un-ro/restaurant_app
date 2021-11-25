156ddf3c8fc4b16e5f70207a942b240011366f66
Unero <ramadhanbhg@gmail.com>
2021-11-25 07:28:33 +0700

    fix: Setting reset state

--- lib/ui/setting_page.dart
+++ lib/ui/setting_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/data/provider/setting_provider.dart';
 
 class SettingPage extends StatelessWidget {
+  static const routeName = '/setting';
   const SettingPage({Key? key}) : super(key: key);
 
   @override
@@ -10,8 +13,25 @@ class SettingPage extends StatelessWidget {
         title: Text('Settings'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Settings Page'),
+      body: ChangeNotifierProvider(
+        create: (_) => SettingProvider().setup(),
+        child: Consumer<SettingProvider>(
+          builder: (context, provider, _) {
+            return ListTile(
+              title: Text('Notification'),
+              subtitle: Text('Random Restaurant at 11 AM'),
+              trailing: Container(
+                margin: const EdgeInsets.all(16.0),
+                child: Switch.adaptive(
+                  value: provider.isScheduled,
+                  onChanged: (value) async {
+                    await provider.setScheduled(value);
+                  },
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
